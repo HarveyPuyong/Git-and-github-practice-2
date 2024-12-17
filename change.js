54a04f4c99c7a44041740e21dd7160e32edcd033
HarveyPuyong <harveyluslos80@gmail.com>
2024-12-17 12:51:18 +0800

    Version 2 restore

--- change.js
+++ change.js
@@ -1,3 +1,1 @@
 console.log('change.js');
-console.log('Version 3');
-connsole.log('Another change version 3');
--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 console.log('index.js');
-console.log('Version 3');
-console.log('Another Change Version3')
+console.log('Version 2')
